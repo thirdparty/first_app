3de0ee092a64ab206c68f1efaa8100e159ec0a7e
thirdparty <siyasoft@gmail.com>
2011-06-20 15:34:21 +0530

    New Project added to Git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'b4969cc071794cf1fd886d5990a388b2e8f2bbe6a2ba75621cdb391eb4704c685bcdc99e957f5606d24c18b3b75afe6e30a92f89d3caf39b2bec4c7a5ab5d781'
